0a5980f8a0e79d94717713f25c49d2fae27756b5
dodz <58638164+mrdodz@users.noreply.github.com>
2023-05-16 22:18:16 +0300

    Create boot.sh

---
+++ boot.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+INFO_LEVEL="\033[0;33m"
+
+function print_banner() {
+  echo "        __          __                              "
+  echo "    ____/ /___  ____/ /___                          "
+  echo "   / __  / __ \/ __  /_  /     OSX Bootsrap Script  "
+  echo "  / /_/ / /_/ / /_/ / / /_             v.1          "
+  echo "  \__,_/\____/\__,_/ /___/                          "
+  echo ""
+}
+
+print_banner
